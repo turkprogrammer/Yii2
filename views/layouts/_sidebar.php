53e44e62c47ab60604b05bdba1a789ebf61b1522
Robert <ostbat43@gmail.com>
2018-08-17 13:25:28 +0500

    вынес сайдбар из общего шаблона

---
+++ views/layouts/_sidebar.php
@@ -0,0 +1,33 @@
+ <div class="recent">
+					 <h3>RECENT POSTS</h3>
+					 <ul>
+					 <li><a href="#">Aliquam tincidunt mauris</a></li>
+					 <li><a href="#">Vestibulum auctor dapibus  lipsum</a></li>
+					
+					 </ul>
+				 </div>
+				 <div class="comments">
+					 <h3>RECENT COMMENTS</h3>
+					 <ul>
+					
+					 <li><a href="#">Steve Roberts  </a> on <a href="#">HTML5/CSS3</a></li>
+					 </ul>
+				 </div>
+				 <div class="clearfix"></div>
+				 <div class="archives">
+					 <h3>ARCHIVES</h3>
+					 <ul>
+					 <li><a href="#">October 2013</a></li>
+					 <li><a href="#">September 2013</a></li>
+					
+					 </ul>
+				 </div>
+				 <div class="categories">
+					 <h3>CATEGORIES</h3>
+					 <ul>
+					 <li><a href="#">Vivamus vestibulum nulla</a></li>
+					 <li><a href="#">Integer vitae libero ac risus e</a></li>
+					
+					 </ul>
+				 </div>
+				 <div class="clearfix"></div>
