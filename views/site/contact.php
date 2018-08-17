b9752a39ba2ad7361a5b293ab295e4c04872b39c
Robert <ostbat43@gmail.com>
2018-08-17 20:14:04 +0500

    поля форма чуть увеличил

--- views/site/contact.php
+++ views/site/contact.php
@@ -39,7 +39,7 @@ $this->params['breadcrumbs'][] = $this->title;
         </p>
 
         <div class="row">
-            <div class="col-lg-5">
+            <div class="col-lg-10">
 
                 <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
 
