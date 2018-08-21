11847a7ead6754e99cbe950f0892a8d05b1549e9
Robert <ostbat43@gmail.com>
2018-08-21 17:58:49 +0500

    вывод линка на категорию с переходом с главной и со страницы блога используя связи моделей

--- views/post/index.php
+++ views/post/index.php
@@ -30,13 +30,16 @@ $this->params['breadcrumbs'][] = $this->title;
 						 <div class="post-info">
                                                      <h4><a href="<?= \yii\helpers\Url::to(['post/view', 'id'=>$post->id])?>"><?=$post->title ?></a>  <span class="label label-default">July 30, 2014 / 27 Comments</span></h4>
 						 <p><?= $post->excerpt ?></p>
-						 <!--   yii2-social-share A beautiful social share buttons for yii2.-->
-<?= \imanilchaudhari\socialshare\ShareButton::widget([
-        'style'=>'horizontal',
-        'networks' => ['facebook','googleplus','linkedin','twitter'],
-        'data_via'=>'imanilchaudhari', //twitter username (for twitter only, if exists else leave empty)
-]); ?>
-						 <a href="<?= \yii\helpers\Url::to(['post/view', 'id'=>$post->id])?>"><span></span>READ MORE</a>
+                                                 <!--   yii2-social-share A beautiful social share buttons for yii2.-->
+                                                         <?=
+                                                         \imanilchaudhari\socialshare\ShareButton::widget([
+                                                             'style' => 'horizontal',
+                                                             'networks' => ['facebook', 'googleplus', 'linkedin', 'twitter'],
+                                                             'data_via' => 'imanilchaudhari', //twitter username (for twitter only, if exists else leave empty)
+                                                         ]);
+                                                         ?>
+						 <a href="<?= \yii\helpers\Url::to(['post/view', 'id'=>$post->id])?>"><span></span>Читать</a>  
+                                <a class="btn btn-default"  role="button" href="<?= \yii\helpers\Url::to(['category/view', 'id' => $post->category->id]) ?>"><?= $post->category->name ?></a> 
 						 </div>
 					 </div>
 					
--- views/site/index.php
+++ views/site/index.php
@@ -1,5 +1,4 @@
 <?php
-
 /* @var $this yii\web\View */
 
 $this->title = 'My Yii Application';
@@ -17,40 +16,49 @@ $this->title = 'My Yii Application';
     <div class="body-content">
 
         <div class="row">
-            
-            
-             
-            
-            
-           <?php //echo $text ;?>
+
+
+
+
+
+            <?php //echo $text ; ?>
             <br/>
-            
-            <?php if(!empty ($posts) ) :?>
-            
-            <?php foreach ($posts as $post) :?>
-             <div class="content-grid">					 
-					 <div class="content-grid-info">
-						 <!--<a href="/"><img src="images/post1.jpg" title="" /></a> -->
-						
-						 <div class="post-info">
-                                                     <h4><a href="<?= \yii\helpers\Url::to(['post/view', 'id'=>$post->id])?>"><?=$post->title ?></a>  <span class="label label-default">July 30, 2014 / 27 Comments</span></h4>
-						 <p><?= $post->excerpt ?></p>
-						 
-						 <a href="site/about/"><span></span>READ MORE</a>
-						 </div>
-					 </div>
-					
-					 
-				 </div>
-            
-            <?php endforeach;?>
-            <?= yii\widgets\LinkPager::widget(['pagination' => $pages]) ;?>
-            <?php endif ;?>
-            
-            
-            
-            
-          
+
+            <?php if (!empty($posts)) : ?>
+
+                <?php foreach ($posts as $post) : ?>
+                    <div class="content-grid">					 
+                        <div class="content-grid-info">
+                                <!--<a href="/"><img src="images/post1.jpg" title="" /></a> -->
+
+                            <div class="post-info">
+                                <h4><a href="<?= \yii\helpers\Url::to(['post/view', 'id' => $post->id]) ?>"><?= $post->title ?></a>  <span class="label label-default">July 30, 2014 / 27 Comments</span></h4>
+                                <p><?= $post->excerpt ?></p>
+                                <!--   yii2-social-share A beautiful social share buttons for yii2.-->
+                                <?=
+                                \imanilchaudhari\socialshare\ShareButton::widget([
+                                    'style' => 'horizontal',
+                                    'networks' => ['facebook', 'googleplus', 'linkedin', 'twitter'],
+                                    'data_via' => 'imanilchaudhari', //twitter username (for twitter only, if exists else leave empty)
+                                ]);
+                                ?>
+
+                                <a href="<?= \yii\helpers\Url::to(['post/view', 'id' => $post->id]) ?>"><span></span>Читать</a>  
+                                <a class="btn btn-default"  role="button" href="<?= \yii\helpers\Url::to(['category/view', 'id' => $post->category->id]) ?>"><?= $post->category->name ?></a> 
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                <?php endforeach; ?>
+    <?= yii\widgets\LinkPager::widget(['pagination' => $pages]); ?>
+<?php endif; ?>
+
+
+
+
+
         </div>
 
     </div>
