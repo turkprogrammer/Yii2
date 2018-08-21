2fb00012daf02fdcce78d62bbc7bb4a5b2c1d956
Robert <ostbat43@gmail.com>
2018-08-21 17:17:47 +0500

    HasOne in Post model link

--- models/Post.php
+++ models/Post.php
@@ -1,18 +1,23 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 namespace app\models;
+
 use yii\db\ActiveRecord;
 
 class Post extends ActiveRecord {
-    public static function tablename ()
-    {
+
+    public static function tablename() {
         return 'post';
     }
-    
+
+    public function getCategory() {
+        return $this->hasOne(Category::className(), ['id' => 'category_id']); //возвращаем выбранный тип связи, савязываем таблицу по полям ['id'=>'category_id']
+    }
+
 }
