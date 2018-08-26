781ad783327fd95704e268399aa8ec3b7532a1eb
Robert <ostbat43@gmail.com>
2018-08-27 02:31:47 +0500

    return \Yii::$app->user->identity->role === 'admin'; // проверка, если роль админ то возвращается тру, в противном случае фальш выдаем 403 или вариант с ридиректом                             /*if (\Yii::$app->user->identity->role !== 'admin') {                                 return \Yii::$app->getResponse()->redirect('/');                             } else {                                 return 1;                             }*/                         }

--- modules/admin/Module.php
+++ modules/admin/Module.php
@@ -1,14 +1,15 @@
 <?php
 
 namespace app\modules\admin;
+
 use yii\web\Controller;
 use yii\filters\AccessControl;
 
 /**
  * admin module definition class
  */
-class Module extends \yii\base\Module
-{
+class Module extends \yii\base\Module {
+
     /**
      * {@inheritdoc}
      */
@@ -17,16 +18,16 @@ class Module extends \yii\base\Module
     /**
      * {@inheritdoc}
      */
-    public function init()
-    {
+    public function init() {
         parent::init();
 
         // custom initialization code goes here
     }
- 
+
     /* определяет правило, согласно которому доступ к модулю (‘allow’ => true) имеют только авторизованные пользователи
-     *  (‘roles’ => ['@']). Знак @ — это специальный символ, обозначающий авторизованного пользователя*/
-    public function behaviors(){
+     *  (‘roles’ => ['@']). Знак @ — это специальный символ, обозначающий авторизованного пользователя */
+
+    public function behaviors() {
         return [
             'access' => [
                 'class' => \yii\filters\AccessControl::className(),
@@ -34,9 +35,18 @@ class Module extends \yii\base\Module
                     [
                         'allow' => true,
                         'roles' => ['@'],
+                        'matchCallback' => function($rule, $action) {
+                            return \Yii::$app->user->identity->role === 'admin'; // проверка, если роль админ то возвращается тру, в противном случае фальш
+                            /*if (\Yii::$app->user->identity->role !== 'admin') {
+                                return \Yii::$app->getResponse()->redirect('/');
+                            } else {
+                                return 1;
+                            }*/
+                        }
                     ],
                 ],
             ],
         ];
     }
+
 }
