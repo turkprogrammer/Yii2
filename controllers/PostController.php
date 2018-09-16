f0ef8bc5b97712446926e05ed788bd868d9a4705
Robert <ostbat43@gmail.com>
2018-09-16 16:31:10 +0500

    Вывод вложенного комментария к записи

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -1,30 +1,37 @@
 <?php
 
 namespace app\controllers;
+
 use app\models\Post;
+use app\models\Comments;
 
+class PostController extends AppController {
 
-class PostController extends AppController
-{
-    public function actionIndex()
-    {
+    public function actionIndex() {
         $query = Post::find()->select('id, title, excerpt, category_id, created, updated, image')->orderBy('id desc');
-        $pages = new \yii\data\Pagination(['totalCount' => $query->count(), 'pageSize'=> 2, 'pageSizeParam' => false, 'forcePageParam' => false]); //Пагинация, сичтаем общее колво записей и передаем в парамтр вывода лимит на страницу
-        $posts = $query->offset($pages->offset)->limit($pages->limit)->all();    
-         
-        return $this->render('index', compact('posts','pages'));
+        $pages = new \yii\data\Pagination(['totalCount' => $query->count(), 'pageSize' => 2, 'pageSizeParam' => false, 'forcePageParam' => false]); //Пагинация, сичтаем общее колво записей и передаем в парамтр вывода лимит на страницу
+        $posts = $query->offset($pages->offset)->limit($pages->limit)->all();
+
+        return $this->render('index', compact('posts', 'pages'));
     }
-    
-    public function actionView()
-            
-    {
+
+    public function actionView() {
         $id = \Yii ::$app->request->get('id');
-        $post = Post::findOne($id);    
-        if (empty($post)) throw new \yii\web\HttpException(404, 'Mazafaka'); // если пост удален то выбрасываем исключение 404
+        $post = Post::findOne($id);
+        if (empty($post))
+            throw new \yii\web\HttpException(404, 'Mazafaka'); // если пост удален то выбрасываем исключение 404
+        $comments = Comments::find()->where(['post_id' => $post])->indexBy('id')->asArray()->all();
+        $tree = [];
+        foreach ($comments as $id => &$node) {
+            if (!$node['parent_id']) {
+                $tree[$id] = &$node;
+            } else {
+                $comments[$node['parent_id']]['childs'][$node['id']] = &$node;
+            }
+        }
+        self::debug($tree); exit();
         $this->setMeta($post->title, $post->keywords, $post->description);
         return $this->render('view', compact('post'));
-       
     }
-
 
 }
---
+++ models/Comments.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "comments".
+ *
+ * @property int $id
+ * @property int $parent_id
+ * @property int $post_id
+ * @property string $username
+ * @property string $text
+ */
+class Comments extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'comments';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['parent_id', 'post_id', 'username', 'text'], 'required'],
+            [['parent_id', 'post_id'], 'integer'],
+            [['text'], 'string'],
+            [['username'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'parent_id' => 'Parent ID',
+            'post_id' => 'Post ID',
+            'username' => 'Username',
+            'text' => 'Text',
+        ];
+    }
+}
