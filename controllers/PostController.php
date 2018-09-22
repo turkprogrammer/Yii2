d23ce28ce58ffd2f79ac5db9f44ba51531077ae0
Robert <ostbat43@gmail.com>
2018-09-22 19:57:32 +0500

    добавление формы комментария к записи, отображение вормы, если !Yii::$app->user->isGuest

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace app\controllers;
-
+use Yii;
 use app\models\Post;
 use app\models\Comments;
+use app\models\User;
 
 class PostController extends AppController {
 
@@ -20,6 +21,10 @@ class PostController extends AppController {
         $post = Post::findOne($id);
         if (empty($post))
             throw new \yii\web\HttpException(404, 'Mazafaka'); // если пост удален то выбрасываем исключение 404
+            /*
+             * вытаскиваем комментарии в массиве
+             */
+        $this->setMeta($post->title, $post->keywords, $post->description);
         $comments = Comments::find()->where(['post_id' => $post])->indexBy('id')->asArray()->all();
         $tree = [];
         foreach ($comments as $id => &$node) {
@@ -29,9 +34,21 @@ class PostController extends AppController {
                 $comments[$node['parent_id']]['childs'][$node['id']] = &$node;
             }
         }
-        self::debug($tree); exit();
-        $this->setMeta($post->title, $post->keywords, $post->description);
-        return $this->render('view', compact('post'));
+        $comments = new Comments();
+        if ($comments->load(Yii::$app->request->post())) {
+            $user = User::findOne(Yii::$app->user->id);
+            $comments->post_id = $post->id;
+            $comments->username = $user->username;
+           //self::debug($tree); exit();
+            $comments->save();         
+            return $this->redirect(['view', 'id' => $post->id]);
+            
+        } else {
+            return $this->render('view', compact('post', 'tree', 'comments'));
+        }
+        //self::debug($tree); exit();
+        //$this->setMeta($post->title, $post->keywords, $post->description);
+        //return $this->render('view', compact('post','tree','comments'));
     }
 
 }
--- views/post/view.php
+++ views/post/view.php
@@ -1,5 +1,10 @@
 <?php
 /* @var $this yii\web\View */
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\widgets\ActiveForm;
+
 $this->title = $post->title;
 $this->params['breadcrumbs'][] = $post->title;
 ?>
@@ -21,3 +26,81 @@ $this->params['breadcrumbs'][] = $post->title;
     'data_via' => 'imanilchaudhari', //twitter username (for twitter only, if exists else leave empty)
 ]);
 ?>
+</br>
+</br>
+<!--Коментарии-->
+
+<div class="comments">
+    <h3 class="title-comments">Комментарии</h3>
+    <ul class="media-list">
+        <!-- Комментарий (уровень 1) -->
+<?php foreach ($tree as $comm) : ?>
+            <li class="media">
+                <div class="media-left">
+
+                    <span class="media-object img-rounded"> <i class="fa fa-comment-o" aria-hidden="true"></i> </span>
+
+                </div>
+                <div class="media-body">
+                    <div class="media-heading">
+                        <div class="author"><?= $comm['username'] ?></div>
+                    </div>
+                    <div class="media-text text-justify"><?= $comm['text'] ?></div>
+                    <div class="footer-comment">
+                        <span class="comment-reply">
+                            <a href="#comment-text" class="reply" data-comment="<?= $comm['id'] ?>">ответить</a>
+                        </span>
+                    </div>
+
+                    <!-- Вложенный медиа-компонент (уровень 2) -->
+    <?php if (isset($comm['childs'])) : ?>
+        <?php foreach ($comm['childs'] as $child) : ?>
+                            <div class="media">
+                                <div class="media-left">
+                                    <span class="media-object img-rounded"> <i class="fa fa-comments-o" aria-hidden="true"></i> </span>
+                                </div>
+                                <div class="media-body">
+                                    <div class="media-heading">
+                                        <div class="author"><?= $child['username'] ?></div>
+                                    </div>
+                                    <div class="media-text text-justify"><?= $child['text'] ?></div>
+                                    <!--<div class="footer-comment">
+
+                                        </span>
+                                        <span class="comment-reply">
+                                        <a href="#" class="reply">ответить</a>
+                                        </span>
+                                    </div>-->
+
+                                    <!-- Вложенный медиа-компонент (уровень 3) -->
+
+                                </div>
+                            </div>
+        <?php endforeach; ?>
+    <?php endif; ?>
+                    <!-- Конец вложенного комментария (уровень 2) -->
+
+                </div>
+            </li>
+            <!-- Конец комментария (уровень 1) -->
+<?php endforeach; ?>
+    </ul>
+      <?php if(!Yii::$app->user->isGuest) : ?>
+ <?php $form = ActiveForm::begin() ?>
+
+        <?= $form->field($comments, 'parent_id')->hiddenInput(['value' => 0])->label(false) ?>
+
+        <?= $form->field($comments, 'text')->textarea(['rows' => 6]) ?>
+
+        <div class="form-group">
+            <?= Html::submitButton('Коментировать', ['class' => 'btn btn-default']) ?>
+        </div>
+
+
+        <?php ActiveForm::end() ?>
+    
+   
+       
+                      
+        <?php endif; ?>
+</div>
