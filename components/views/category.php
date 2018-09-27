69cc43dbddae30d2d65b11646705b4aafd862a6e
Robert <ostbat43@gmail.com>
2018-09-27 21:24:27 +0500

    вывод количесвта статей в виджете категорий

--- components/views/category.php
+++ components/views/category.php
@@ -1,9 +1,15 @@
+<?php
+use app\models\Post; // подключаем модель Постов
+?>
+
 
 <?php if (!empty($cats)) : ?>
     <ul>
         <?php foreach ($cats as $cat) : ?>
+        <?php  $count = Post::find()->where(['category_id' => $cat['id']])->count(); ?>
             <li>  
-              <i class="glyphicon glyphicon-list" aria-hidden="true"></i> <a href="<?= yii\helpers\Url::to(['category/view', 'id'=>$cat['id']]) ?>"><?= $cat['name'] ?> </a> 
+              <i class="glyphicon glyphicon-list" aria-hidden="true"></i> <a href="<?= yii\helpers\Url::to(['category/view', 'id'=>$cat['id']]) ?>"><?= $cat['name'] ?> </a>
+         <span class="badge"><?=$count?></span>
             </li>                    
         <?php endforeach; ?>
     </ul>
