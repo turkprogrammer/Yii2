b339810bf714ef727131644e2dfc3fcb8c9b1752
Robert <ostbat43@gmail.com>
2018-10-18 22:04:08 +0500

    вывод userIP

--- views/layouts/_sidebar.php
+++ views/layouts/_sidebar.php
@@ -1,23 +1,26 @@
-
+<?php use \yii\Helpers\Html;?>
 <div class="categories">
     <h3>Категории </h3> 
-    <?= app\components\CatsWidget::widget() ?>
+ <div class="jumbotron">   <?= app\components\CatsWidget::widget() ?></div>
 </div>
 <br/>
 <h3>Тестовый виджет</h3>
 <br/>
-<span class="label label-danger"><?= app\components\TestWidget::widget() ?></span>
+
+<div class="jumbotron"><?= app\components\TestWidget::widget() ?></div>
 <hr/>
 <br/>
 <h3>Последние записи</h3>
 <br/>
 <?= app\components\ListWidget::widget() ?>
+
 <hr/>
 <br/>
 
 <div class="panel panel-default">
     <div class="panel-body">
         <a href="<?php echo yii\helpers\Url::to('/admin') ?>"> Админка <i class="glyphicon glyphicon-user" aria-hidden="true"></i></a>
+		<?= Html::tag('code', 'Your IP is '	. Yii::$app->request->userIP);?>
         <br/>
         <?php if (!Yii::$app->user->isGuest) : ?>
             <a href="<?= yii\helpers\Url::to('/site/logout') ?>"><?= Yii::$app->user->identity['username'] ?> (Выход) <i class="glyphicon glyphicon-log-out" aria-hidden="true"></i></a>
@@ -29,8 +32,9 @@
     </div>
 </div>
 
-
+<div class="jumbotron1"><?//= app\components\CommentsWidget::widget() ?></div>
 
 <?php //var_dump(Yii::$app->user->identity) ;?>
+
 
 
